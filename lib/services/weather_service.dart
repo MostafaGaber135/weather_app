ebc6a275f094587c53c558232e9d225e6662651d
Mostafa Gaber Ahmed Hussien <119273107+MostafaGaber135@users.noreply.github.com>
2023-12-13 20:59:15 +0200

    trigger request

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -5,7 +5,7 @@ import 'package:weather_app/models/weather_model.dart';
 
 class WeatherService{
   final String apiKey = 'b3244f8c4e8a49b4ab3171918231012';
-  final String baseUrl = 'http://api.weatherapi.com/v1';
+  final String baseUrl = 'https://api.weatherapi.com/v1';
   final Dio dio;
 
     WeatherService(this.dio);
@@ -21,6 +21,6 @@ class WeatherService{
       } catch(e) {
         log(e.toString());
         throw Exception('Oops there was an error, try later');
-      }
     }
+  }
 }
--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,4 +1,9 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
@@ -12,8 +17,9 @@ class SearchView extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Center(
           child: TextField(
-            onChanged: (value){
-              
+            onSubmitted: (value) async {
+            WeatherModel weatherModel = await WeatherService(Dio()).getCurrentWeather(cityName: value );
+            log(weatherModel.cityName);
             },
             decoration: const InputDecoration(
               contentPadding: EdgeInsets.symmetric(
